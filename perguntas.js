579865cc2afbd694be647a698d8965e5206a5b7d
eulequin <elexandro.filho@escola.pr.gov.br>
2025-06-11 01:42:40 +0000

    Configurações finais do projeto

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Cristiano Ronaldo',
+    'Qual foi o primeiro clube profissional de Cristiano Ronaldo?',
+    'O primeiro clube profissional de Cristiano Ronaldo foi o Sporting Clube de Portugal.'
+)
+
+criaCartao(
+    'Cristiano Ronaldo',
+    'Em que ano Cristiano Ronaldo fez sua estreia pelo Manchester United?',
+    'Ele estreou pelo Manchester United em 2003, aos 18 anos.'
+)
+
+criaCartao(
+    'Cristiano Ronaldo',
+    'Quantos filhos Cristiano Ronaldo tem?',
+    'Até 2025, Cristiano Ronaldo tem 5 filhos.'
+)
+
+criaCartao(
+    'Cristiano Ronaldo',
+    'Qual é a comemoração mais famosa de CR7?',
+    'Sua comemoração mais icônica é o salto com o giro no ar seguido do grito “Siiii!” com os braços estendidos.'
+)
+
+criaCartao(
+    'Cristiano Ronaldo',
+    'Cristiano Ronaldo já jogou em quantos países diferentes como profissional?',
+    'Ele já jogou em cinco países: Portugal, Inglaterra, Espanha, Itália e Arábia Saudita.'
+)
+
+criaCartao(
+    'Cristiano Ronaldo',
+    'Quantas Bolas de Ouro CR7 já ganhou?',
+    'Cristiano Ronaldo venceu a Bola de Ouro 5 vezes (até 2025).'
+)
+
+criaCartao(
+    'Cristiano Ronaldo',
+    'Qual o significado da sigla “CR7”?',
+    '“CR” são as iniciais de Cristiano Ronaldo, e “7” é o número da camiseta que ele consagrou ao longo da carreira.'
+)
+
+criaCartao(
+    'Cristiano Ronaldo',
+    'CR7 já marcou mais de quantos gols em sua carreira?',
+    'Ele já superou a marca de mais de 900 gols em toda a sua carreira'
+)
+
+criaCartao(
+    'Cristiano Ronaldo',
+    'Cristiano Ronaldo é o maior artilheiro da história da Seleção Portuguesa?',
+    'Sim, ele é o maior artilheiro da história de Portugal, com mais de 120 gols pela seleção.'
+)
